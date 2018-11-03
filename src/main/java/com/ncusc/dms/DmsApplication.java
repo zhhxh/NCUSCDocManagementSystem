591a0749ca0bfe35e30be200f8bafe9b2dd66430
WANGHAO <1727386617@qq.com>
2018-11-03 19:51:26 +0800

    init

---
+++ src/main/java/com/ncusc/dms/DmsApplication.java
@@ -0,0 +1,12 @@
+package com.ncusc.dms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmsApplication.class, args);
+	}
+}
