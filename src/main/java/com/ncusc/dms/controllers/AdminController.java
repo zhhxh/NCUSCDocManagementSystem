10de336a3afe2b8934e673299e6cc651c91300d1
WANGHAO <1727386617@qq.com>
2018-11-04 23:36:50 +0800

    add static resources test

--- src/main/java/com/ncusc/dms/controllers/AdminController.java
+++ src/main/java/com/ncusc/dms/controllers/AdminController.java
@@ -32,6 +32,6 @@ public class AdminController {
 
         List<Admin> adminList= adminService.list();
         model.addAttribute("adminList", adminList);
-        return "listAdmin";//templates下的listAdmin
+        return "test/Admin/listAdmin";//istAdmin.html的路径
     }
 }
--- src/test/java/com/ncusc/dms/DmsApplicationTests.java
+++ src/test/java/com/ncusc/dms/DmsApplicationTests.java
@@ -1,7 +1,12 @@
 package com.ncusc.dms;
 
+import java.util.List;
+
+import com.ncusc.dms.mapper.AdminMapper;
+import com.ncusc.dms.pojo.Admin;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -11,6 +16,24 @@ public class DmsApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+	@Autowired
+	AdminMapper adminMapper;
+	@Test
+	public void testAdminMapper(){
+		List<Admin> list = adminMapper.list();
+		for (int i = 0;i<list.size();i++){
+			Admin admin = list.get(i);
+			System.out.println("ID:"+admin.getaId());
+			System.out.println("Password:"+admin.getaPassword());
+			System.out.println("Name:"+admin.getaName());
+			System.out.println("Sex:"+admin.getaSex());
+			System.out.println("PhoNum:"+admin.getaPhoNum());
+			System.out.println("Addr:"+admin.getaAddr());
+			System.out.println("Limit:"+admin.getaLimit());
+			System.out.println("Date:"+admin.getaDate());
+		}
+
 	}
 
 }
